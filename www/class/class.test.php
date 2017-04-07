b71de8578f3592abde28af06d2167d2a31833cdb
simple777 <user@example.com>
2017-04-07 13:31:13 +0300

    Работа с классами

--- www/class/class.test.php
+++ www/class/class.test.php
@@ -5,7 +5,7 @@ class Article
     public $title;
     public $text;
 
-    public function __construct( $title, $text )
+    public function __construct( $title, $text = '' )
     {
         $this->title = $title;
         $this->text = $text;
--- www/index.php
+++ www/index.php
@@ -5,4 +5,7 @@ require_once __DIR__.'/class/class.test.php';
 $art = new Article( 'Важная новость', 'Текст важной новости' );
 $art->view();
 
+$art = new Article( 'Просто новость' );
+$art->view();
+
 ?>
